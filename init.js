618e630905ff545544f8d2e46617e14f8b8cdd12
MFuentes01 <megax.yt.uy@gmail.com>
2023-08-18 10:55:32 -0300

    js

---
+++ init.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const pokeInput = document.getElementById("pokeInput");
+    const searchButton = document.getElementById("searchButton");
+    const pokeInfo = document.getElementById("pokeInfo");
+    
+    
+
+
+
+    searchButton.addEventListener("click", () => {
+    
+      let pokeDato = pokeInput.value;
+        
+
+      const url = `https://pokeapi.co/api/v2/pokemon/${pokeDato}`;
+  
+      fetch(url)
+        .then(response => response.json())
+        .then(data => {
+          pokeInfo.innerHTML = `
+            <h2>${data.name}</h2>
+            <img src="${data.sprites.front_default}" alt="${data.name}">
+            <p>Height: ${data.height}</p>
+            <p>Weight: ${data.weight}</p>
+          `;
+        })
+        .catch(error => {
+          console.error("Hubo un error:", error);
+        });
+    });
+  });
