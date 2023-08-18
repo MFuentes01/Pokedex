704844f2b6bc089a29720df17363947f11f604ee
MFuentes01 <megax.yt.uy@gmail.com>
2023-08-18 10:16:34 -0300

    Second commit

--- init.js
+++ init.js
@@ -1,9 +1,23 @@
 document.addEventListener("DOMContentLoaded", function(){
-const cont = document.getElementById("cont")
+const cont = document.getElementById("cont");
+let poke_nmbr = 25
+const poke_data = "https://pokeapi.co/api/v2/pokemon/${poke_nmbr}";
+
+
+fetch("poke_data")
+.then(response => response.json())
+.then(data => {
+    const pokemon = document.createElement("div");
+    pokemon.className = "formato";
+
+    pokemon.innerHTML = pokemon.name
 
 
 
-
-
+    cont.appendChild(pokemon);
+})
+})
+.catch(error => {
+    console.error("Error al obtener el JSON:", error);
 })
 
